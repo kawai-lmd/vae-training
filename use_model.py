813a461ee3eb66b77d47a5da7abde891b14e3e35
kawai-lmd <kawai@lmd.ist.hokudai.ac.jp>
2023-05-11 00:18:28 +0900

    mod use_model

--- use_model.py
+++ use_model.py
@@ -1,4 +1,7 @@
 # Load saved VAE model
 from keras.models import load_model
 
-vae = load_model('models/model.h5')
+vae = load_model('models/vae_model.h5')
+encoder = load_model('models/encoder_model.h5')
+decoder = load_model('models/decoder_model.h5')
+
